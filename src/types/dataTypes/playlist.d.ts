692dcaba5de466227b38c0c5e92b545dc9d2b9bf
Ali Hourag <ali.hourag@gmail.com>
2023-07-11 13:32:17 +0200

    fix(playlistType): add owner type to playlistst

--- src/types/dataTypes/playlist.d.ts
+++ src/types/dataTypes/playlist.d.ts
@@ -4,6 +4,9 @@ export interface PlaylistsType {
     id: string,
     name: string,
     isFollowed: boolean,
+    // If owner is app, then owner = "Apollofy"
+    // If owner is user, then owner = userName
+    owner: string,
     thumbnail: string,
     description: string,
     tracks: TrackType[]
