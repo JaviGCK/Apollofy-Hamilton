33b524d0c75d5486eb3ef1fc8a22e74481807df5
Javier García <javigcpgr@gmail.com>
2023-07-12 11:01:23 +0200

    feat(icons-navbar): set navbar components

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,27 @@
+import './navBar.css'
+import { BiHomeAlt2, BiSearch, BiSolidHeart } from 'react-icons/bi'
+import { MdLibraryAdd } from 'react-icons/md'
+import { GiAlienStare } from 'react-icons/gi'
+
+
+export const NavBar = () => {
+
+    const iconsNavbar = ["home-icon", "search-icon", "add-icon", "fav-icon", "user-icon"]
+    return (
+        <nav className='navBar-bottom-container'>
+            {iconsNavbar.map((icon, index) => (
+                <div className='icon-navbar-container'>
+                    <input id={icon} name='icon-navbar-bottom' type='radio' className='input-navbar-bottom' />
+                    <label htmlFor={icon} className='label-navbar-bottom'>
+                        {index === 0 && <BiHomeAlt2 className='icons-navbar'/> }
+                        {index === 1 && <BiSearch className='icons-navbar'/>}
+                        {index === 2 && <MdLibraryAdd className='icons-navbar'/>}
+                        {index === 3 && <BiSolidHeart className='icons-navbar'/>}
+                        {index === 4 && <GiAlienStare className='icons-navbar'/>}
+
+                    </label>
+                </div>
+            ))}
+        </nav>
+    )
+}
