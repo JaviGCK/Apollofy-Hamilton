f78e3597d5cd1801285485324bcee751233b3a0c
Matias Alaimo <matias.chiappa@hotmail.com>
2023-07-13 13:18:20 +0200

    Feat(genreList Buttons, fetchApi, modified .json file, genreList)

--- src/components/searchPage/genresList/GenresList.tsx
+++ src/components/searchPage/genresList/GenresList.tsx
@@ -1,9 +1,28 @@
+import { GenreButton } from "../genreButton/genreButton";
+import { fetchGenres } from "../../../api/fetchApi";
+import { GenreType } from "../../../types/dataTypes/genre";
+import { useState, useEffect } from "react";
 import "./genresList.css"
 
+
 export const GenresList = () => {
+    const [genres, setGenres] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetchGenres();
+            setGenres(data);
+        }
+        fetchData();
+    }, [])
     return (
         <section className="genreslist-container">
-
+        <p className="top-clases-genres">Your top genres</p>
+            {genres.map((genre: GenreType) => {
+                return(
+                    <GenreButton key={genre.id} id={genre.id} name={genre.name} imageUrl={genre.imageUrl} btnColor={genre.btnColor}/>
+                )
+            })}
+            <div className="white-space"></div>
         </section>
     )
 }
