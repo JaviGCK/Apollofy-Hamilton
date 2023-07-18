2a87ed05b635cdf39eb88f046c8cc97b1645839b
Javier García <javigcpgr@gmail.com>
2023-07-18 16:07:53 +0200

    fix(listDetailPage): change position style to flex style

--- src/pages/listDetailPage/ListDetailPage.tsx
+++ src/pages/listDetailPage/ListDetailPage.tsx
@@ -10,10 +10,10 @@ import { ListDetailProps } from '../../types/propTypes/listDetailProps';
 export const ListDetailPage: FC<ListDetailProps> = ({ ...props }) => {
 
     const [album, setAlbum] = useState<AlbumType | null>(null);
-
+/**${props.albumId} */
     useEffect(() => {
         const api = async () => {
-            const data = await fetch(`http://localhost:3001/albums?id=${props.albumId}`);
+            const data = await fetch(`http://localhost:3001/albums?id=4yP0hdKOZPNshxUOjY0cZj`);
             const albumFetched = await data.json();
             setAlbum(albumFetched[0]);
         };
@@ -25,11 +25,13 @@ export const ListDetailPage: FC<ListDetailProps> = ({ ...props }) => {
             {album && <div className="list-detail-page-container">
                 <div className="list-detail-heading">
                     <FaAngleLeft className="list-detail-angle-btn" />
+
                     <img
                         className="list-detail-img"
                         src={album?.imageUrl}
                         alt={`Image or Cover of ${album?.name}`}
                     />
+
                     <div className="list-detail-dashboard">
                         <AiOutlinePlusCircle className="list-detail-add-btn" />
                         <BiSolidHeart className="list-detail-heart-btn" />
@@ -37,6 +39,7 @@ export const ListDetailPage: FC<ListDetailProps> = ({ ...props }) => {
                             <BiPlay className="list-detail-play-btn" />
                         </span>
                     </div>
+                    
                 </div>
                 <div className="list-detail-container-items">
                     {album.tracks?.map((track) => (
