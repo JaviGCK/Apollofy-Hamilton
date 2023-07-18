fc5a109f62cf977252235550e8f167f1f6a78ddd
Luis Jover <luisjoverdev@gmail.com>
2023-07-18 17:59:44 +0200

    fix(Layout): fix Layout after merge

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,11 +1,13 @@
 
 import { Outlet } from 'react-router-dom'
 import { NavBar } from '../components/navbar/NavBar'
+import { SoundBar } from '../components/soundBar/SoundBar'
 
 export const Layout = () => {
   return (
     <>
-      <Outlet/>
+      <Outlet />
+      <SoundBar />
       <NavBar />
     </>
   )
