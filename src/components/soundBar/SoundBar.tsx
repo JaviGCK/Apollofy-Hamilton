ab910c7ac319eb9cee4fbc582013b042541a36b1
Luis Jover <luisjoverdev@gmail.com>
2023-07-18 14:06:44 +0200

    feat(MusicPlayer): progress bar deploy a full player page with new feats and options

--- src/components/soundBar/SoundBar.tsx
+++ src/components/soundBar/SoundBar.tsx
@@ -1,34 +1,64 @@
-import { useState, useRef, useEffect } from "react"
+import { useState, useRef, useEffect, useLayoutEffect } from "react"
 import "./soundBar.css"
 import { SoundPlayer } from "./soundPlayer/SoundPlayer"
 import { CurrentTrack } from "../../types/propTypes/soundBarTypes"
 
 
-const testMusic = {
-    name: "Repeat After Me (Interlude)",
-    url: "https://res.cloudinary.com/dqdysl9ep/video/upload/v1689331273/APOLLOFY/apollofy_repeat-after-me-The-weeknd.mp3"
-}
+const testMusic = [
+    {
+        name: "Repeat After Me (Interlude)",
+        url: "https://res.cloudinary.com/dqdysl9ep/video/upload/v1689331273/APOLLOFY/apollofy_repeat-after-me-The-weeknd.mp3",
+        artistName: "Matias LaPara",
+        imageUrl: "https://i.scdn.co/image/ab67616100005174920dc1f617550de8388f368e"
+    },
+    {
+        name: "SECOND TRACK",
+        url: "https://res.cloudinary.com/dqdysl9ep/video/upload/v1689331273/APOLLOFY/apollofy_repeat-after-me-The-weeknd.mp3",
+        artistName: "Matias LaPara",
+        imageUrl: "https://i.scdn.co/image/ab67616100005174920dc1f617550de8388f368e"
+    },
+    {
+        name: "THIRD TRACK",
+        url: "https://res.cloudinary.com/dqdysl9ep/video/upload/v1689331273/APOLLOFY/apollofy_repeat-after-me-The-weeknd.mp3",
+        artistName: "Matias LaPara",
+        imageUrl: "https://i.scdn.co/image/ab67616100005174920dc1f617550de8388f368e"
+    }
+]
 
 export const SoundBar = () => {
 
     const [trackList, setTrackList] = useState(testMusic)
     const [isPlaying, setIsPlaying] = useState(false)
-    const [currentTrack, setCurrentTrack] = useState<CurrentTrack>(testMusic)
+    const [currentTrack, setCurrentTrack] = useState<CurrentTrack>(testMusic[0])
 
     const audioElement = useRef<HTMLAudioElement | null>(null)
 
     const getTrackProgress = () => {
         if (audioElement.current) {
+
             const trackDuration = audioElement.current.duration
             const currentTrackProgress = audioElement.current.currentTime
+
+            console.log(trackDuration)
+            console.log(currentTrackProgress)
             setCurrentTrack({
                 ...currentTrack,
-                "progress": currentTrackProgress / trackDuration * 100,
-                "duration": trackDuration
+                progress: currentTrackProgress / trackDuration * 100,
+                duration: trackDuration
             })
         }
 
     }
+
+    const handleMetaDataLoad = () => {
+        const trackDuration = audioElement.current?.duration
+        setCurrentTrack({
+            ...currentTrack,
+            progress: 0,
+            duration: trackDuration
+        })
+    }
+
     useEffect(() => {
         if (isPlaying && audioElement.current) audioElement.current.play()
         else if (!isPlaying && audioElement.current) audioElement.current.pause()
@@ -36,11 +66,25 @@ export const SoundBar = () => {
 
     }, [isPlaying])
 
+    useEffect(() => {
+        setTrackList(testMusic)
+        audioElement.current?.addEventListener("loadedmetadata", handleMetaDataLoad)
+        return () => { audioElement.current?.removeEventListener("loadedmetadata", handleMetaDataLoad) }
+    }, [])
+
 
     return (
         <div>
-            <audio src={testMusic.url} ref={audioElement} onTimeUpdate={() => getTrackProgress()} />
-            <SoundPlayer trackList={trackList} setTrackList={setTrackList} isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentTrack={currentTrack} setCurrentTrack={setCurrentTrack} audioElement={audioElement} />
+            <audio src={currentTrack.url} ref={audioElement} onTimeUpdate={() => getTrackProgress()} />
+            <SoundPlayer
+                trackList={trackList}
+                setTrackList={setTrackList}
+                isPlaying={isPlaying}
+                setIsPlaying={setIsPlaying}
+                currentTrack={currentTrack}
+                setCurrentTrack={setCurrentTrack}
+                audioElement={audioElement}
+            />
         </div>
     )
 }
--- src/components/soundBar/soundPlayer/SoundPlayer.tsx
+++ src/components/soundBar/soundPlayer/SoundPlayer.tsx
@@ -1,20 +1,29 @@
 
-import { BsFillPlayCircleFill, BsFillPauseCircleFill } from 'react-icons/bs';
+import { BsFillPlayCircleFill, BsFillPauseCircleFill, BsFillSkipStartCircleFill, BsFillSkipEndCircleFill } from 'react-icons/bs';
 import "./soundPlayer.css"
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 
 export const SoundPlayer = ({ ...props }) => {
 
+    const [expandedMenu, setExpandedMenu] = useState(false)
+
+
+    const playerContainer = useRef<HTMLDivElement | null>(null)
+
     const soundBarClickRef = useRef<HTMLDivElement | null>(null)
 
-    const handlePlayPause = () => {
+
+
+
+    const handlePlayPause = (event: any) => {
+        event.stopPropagation()
         props.setIsPlaying(!props.isPlaying)
-        console.log("handlePlay is working")
+
     }
 
     const handleClickNavigation = (event: any) => {
-        if (soundBarClickRef.current) {
+        if (soundBarClickRef.current && props.currentTrack.duration) {
             let soundBarWidth = soundBarClickRef.current.clientWidth
             let offset: number = event.nativeEvent.offsetX
             let offsetPercentage = offset / soundBarWidth * 100
@@ -23,24 +32,160 @@ export const SoundPlayer = ({ ...props }) => {
         }
     }
 
+    const handleToggleMenu = () => {
+        setExpandedMenu(!expandedMenu)
+    }
+
+    const handlePrevTrack = () => {
+
+        const index = props.trackList.findIndex((x: any) => x.name === props.currentTrack.name)
+
+        console.log(index)
+        if (index === 0) {
+            props.setCurrentTrack(props.trackList[props.trackList.length - 1])
+        }
+        else {
+            props.setCurrentTrack(props.trackList[index - 1])
+        }
+
+        props.audioElement.current.currentTime = 0
+    }
+
+    const handleNextTrack = () => {
+        const index = props.trackList.findIndex((x: any) => x.name === props.currentTrack.name)
+
+        console.log(index)
+        if (index === props.trackList.length - 1) {
+            props.setCurrentTrack(props.trackList[0])
+        }
+        else {
+            console.log("entra el el else")
+            props.setCurrentTrack(props.trackList[index + 1])
+        }
+
+        props.audioElement.current.currentTime = 0
+    }
+
+    const formatTime = (seconds: number): string => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = Math.floor(seconds % 60);
+        const formattedTime = `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+        return formattedTime;
+    }
+
+
+
     return (
-        <div className="player-container">
-            <div>
-                <div className="player-title">
-                    <p>{props.currentTrack.name}</p>
-                </div>
-            </div>
-            <div className="content-bar">
-                <div className="navigation-content-bar" ref={soundBarClickRef} onClick={handleClickNavigation}>
-                    <div className="progress-bar" style={{ width: `${props.currentTrack.progress}%`, background: "red", height: "10px" }}>
 
+        <>
+            {!expandedMenu ?
+
+
+                <div ref={playerContainer} className="player-container">
+                    <div className="player-heading" onClick={handleToggleMenu}>
+                        <div className="player-info">
+
+                            <img className="track-image" src={props.currentTrack.imageUrl} alt="" />
+                            <div className='track-info'>
+                                <p>{props.currentTrack.name}</p>
+                                <p>{props.currentTrack.artistName}</p>
+                            </div>
+
+                        </div>
+
+                        <div id="controls-icon" className="controls-container">
+                            {props.isPlaying ? <BsFillPauseCircleFill id="controls-icon" className="controls-icon" onClick={handlePlayPause} /> : <BsFillPlayCircleFill id="controls-icon" className="controls-icon" onClick={handlePlayPause} />}
+
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="controls-container">
-                <BsFillPlayCircleFill onClick={() => handlePlayPause()} />
-            </div>
+                    <div className="content-bar">
+                        <div className="navigation-content-bar" ref={soundBarClickRef} onClick={handleClickNavigation}>
+                            <div className="progress-bar" style={{ width: `${props.currentTrack.progress}%` }}>
 
-        </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                :
+
+                <div ref={playerContainer} className="player-container expanded">
+                    <div className="player-heading heading-expanded">
+                        <img className="track-image image-big" src={props.currentTrack.imageUrl} alt="" />
+                        <div className="player-info info-expanded">
+
+
+                            <div className='track-info'>
+                                <p className='expanded-title'>{props.currentTrack.name}</p>
+                                <p className='expanded-subtitle'>{props.currentTrack.artistName}</p>
+                            </div>
+
+                        </div>
+
+
+                        <div className='track-progress-bar'>
+                            <div className="expanded-content-bar">
+                                <div className="navigation-content-bar" ref={soundBarClickRef} onClick={handleClickNavigation}>
+                                    <div className="progress-bar" style={{ width: `${props.currentTrack.progress}%` }}>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='numeric-progress-container'>
+                                <span>{formatTime(props.currentTrack.progress / 100 * props.currentTrack.duration)}</span>
+                                <span>{formatTime(props.currentTrack.duration)}</span>
+                            </div>
+                        </div>
+
+
+                        <div className="expanded-controls-container">
+                            <BsFillSkipStartCircleFill className="expanded-control-icon" onClick={handlePrevTrack} />
+                            {props.isPlaying ? <BsFillPauseCircleFill className="expanded-control-icon" onClick={handlePlayPause} /> : <BsFillPlayCircleFill className="expanded-control-icon" onClick={handlePlayPause} />}
+                            <BsFillSkipEndCircleFill className="expanded-control-icon" onClick={handleNextTrack} />
+
+                        </div>
+                    </div>
+
+                </div>
+            }
+        </>
+
+
     )
 }
+
+
+
+
+
+
+
+
+
+        // <div ref={playerContainer} className="player-container">
+        //     <div className="player-heading" onClick={handleToggle}>
+        //         <div className="player-info">
+
+        //             <img className="track-image" src={props.currentTrack.imageUrl} alt="" />
+        //             <div className='track-info'>
+        //                 <p>{props.currentTrack.name}</p>
+        //                 <p>{props.currentTrack.artistName}</p>
+        //             </div>
+
+        //         </div>
+
+        //         <div id="controls-icon" className="controls-container">
+        //             {props.isPlaying ? <BsFillPauseCircleFill id="controls-icon" className="controls-icon" onClick={handlePlayPause} /> : <BsFillPlayCircleFill id="controls-icon" className="controls-icon" onClick={handlePlayPause} />}
+
+        //         </div>
+        //     </div>
+        //    <div className="content-bar">
+        //         <div className="navigation-content-bar" ref={soundBarClickRef} onClick={handleClickNavigation}>
+        //             <div className="progress-bar" style={{ width: `${props.currentTrack.progress}%` }}>
+
+        //             </div>
+        //         </div>
+        //     </div>
+
+        // </div>
