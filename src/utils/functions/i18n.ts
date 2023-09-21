6cf8c5ea4e92bd175bf5d972d1448755b2eac24d
Ali Hourag <ali.hourag@gmail.com>
2023-09-21 07:56:08 +0200

    feat(language): change default language to english

--- src/utils/functions/i18n.ts
+++ src/utils/functions/i18n.ts
@@ -4,8 +4,8 @@ import { initReactI18next } from 'react-i18next';
 i18n
     .use(initReactI18next)
     .init({
-        lng: 'es',
-        fallbackLng: 'en',
+        lng: 'en',
+        fallbackLng: 'es',
         interpolation: {
             escapeValue: false
         },
